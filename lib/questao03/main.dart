851d92b4535b0781bc1b178a7c389ff540c6e566
Andre-F-P-Silva <a.n.d.r.e.11.ufcfc@gmail.com>
2026-03-25 21:37:14 -0300

    Adicionando questão 03 - main.dart

---
+++ lib/questao03/main.dart
@@ -0,0 +1,11 @@
+import 'data.dart';
+
+void main() {
+  Data d1 = Data(1, 1, 2000);
+  Data d2 = Data(15, 6, 1995);
+  Data d3 = Data(31, 12, 2024);
+
+  d1.exibirData();
+  d2.exibirData();
+  d3.exibirData();
+}
