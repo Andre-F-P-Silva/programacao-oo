7a9e14e1fa0a9e488b6e309b87fbc3977b1f5972
Andre-F-P-Silva <a.n.d.r.e.11.ufcfc@gmail.com>
2026-03-25 21:32:50 -0300

    Adicionando questão 02 - main.dart

---
+++ lib/questao02/main.dart
@@ -0,0 +1,11 @@
+import 'relogio.dart';
+
+void main() {
+  Relogio r1 = Relogio(8, 30, 0);
+  Relogio r2 = Relogio(14, 45, 30);
+  Relogio r3 = Relogio(17, 15, 15);
+
+  r1.exibirHorario();
+  r2.exibirHorario();
+  r3.exibirHorario();
+}
