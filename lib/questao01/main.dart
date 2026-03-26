8c0533c6f70afaf3972fc2d289a2e5c0c9e0f35b
Andre-F-P-Silva <a.n.d.r.e.11.ufcfc@gmail.com>
2026-03-25 21:18:36 -0300

    Adicionando questao 01 - main.dart

---
+++ lib/questao01/main.dart
@@ -0,0 +1,11 @@
+import 'pessoa.dart';
+
+void main() {
+  Pessoa p1 = Pessoa('Andre', 27);
+  Pessoa p2 = Pessoa('Rayane', 22);
+  Pessoa p3 = Pessoa('Marcia', 46);
+
+  p1.exibirDados();
+  p2.exibirDados();
+  p3.exibirDados();
+}
