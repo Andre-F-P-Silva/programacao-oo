452c2137865ce7e741cce98b7fdcb573c28e0e7e
Andre-F-P-Silva <a.n.d.r.e.11.ufcfc@gmail.com>
2026-03-30 19:14:40 -0300

    Adicionando questão 13 - main.dart

--- lib/questao13/main.dart
+++ lib/questao13/main.dart
@@ -2,7 +2,7 @@ import 'pedido.dart';
 import 'produto.dart';
 
 void main() {
-  Pedido ped1 = Pedido(1, Produto(1, 'Camiseta Athletico Paranaense', 350.0, 1), 1);
+  Pedido ped1 = Pedido(1, Produto(1, 'Camiseta Athletico Paranaense', 350.0, 3), 3);
   Pedido ped2 = Pedido(2, Produto(2, 'Uniforme Completo Athletico Paranaense', 850.0, 5), 5);
 
   ped1.exibirPedido();
