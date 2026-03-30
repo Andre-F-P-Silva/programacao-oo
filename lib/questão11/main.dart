c1d741d5054d8ca0ceeda42643304b7ebddb77cc
Andre-F-P-Silva <a.n.d.r.e.11.ufcfc@gmail.com>
2026-03-30 16:57:10 -0300

    Adicionando questão 11 - main.dart

---
+++ lib/questão11/main.dart
@@ -0,0 +1,19 @@
+
+import 'triangulo.dart';
+
+void main() {
+  Triangulo t1 = Triangulo(3, 4, 5, '*');
+  Triangulo t2 = Triangulo(5, 5, 5, '#');
+
+  print('--- Triângulo 1 ---');
+  t1.exibirResumo();
+  t1.desenharEsquerda();
+  print('');
+  t1.desenharCentralizado();
+
+  print('\n--- Triângulo 2 ---');
+  t2.exibirResumo();
+  t2.desenharEsquerda();
+  print('');
+  t2.desenharCentralizado();
+}
