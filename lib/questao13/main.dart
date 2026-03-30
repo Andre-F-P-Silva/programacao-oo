41550ec5ec79e43c9a78184dad3877f83905b938
Andre-F-P-Silva <a.n.d.r.e.11.ufcfc@gmail.com>
2026-03-30 19:13:40 -0300

    Update main.dart

--- lib/questao13/main.dart
+++ lib/questao13/main.dart
@@ -2,8 +2,8 @@ import 'pedido.dart';
 import 'produto.dart';
 
 void main() {
-  Pedido ped1 = Pedido(1, Produto(1, 'Notebook', 3500.0, 1), 1);
-  Pedido ped2 = Pedido(2, Produto(2, 'Mouse', 80.0, 5), 5);
+  Pedido ped1 = Pedido(1, Produto(1, 'Camiseta Athletico Paranaense', 350.0, 1), 1);
+  Pedido ped2 = Pedido(2, Produto(2, 'Uniforme Completo Athletico Paranaense', 850.0, 5), 5);
 
   ped1.exibirPedido();
   ped2.exibirPedido();
