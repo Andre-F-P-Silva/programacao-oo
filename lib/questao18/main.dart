98ec5a686651caba6291cc8e75249a59cb1ecbca
André Felipe <a.n.d.r.e.11.ufcfc@gmail.com>
2026-04-06 21:23:36 -0300

    Adicionando questão 18 - main.dart

--- lib/questao18/main.dart
+++ lib/questao18/main.dart
@@ -2,9 +2,9 @@ import 'musica.dart';
 import 'playlist.dart';
 
 void main() {
-  Musica m1 = Musica('Ignora', 'Felipe e Rodrigo', 354);
-  Musica m2 = Musica('Deixa Eu', 'Murilo Huff', 391);
-  Musica m3 = Musica('Bebe, Beija e Trai', 'Mayke e Rodrigo, Panda', 482);
+  Musica m1 = Musica('Ignora', 'Felipe e Rodrigo', 254);
+  Musica m2 = Musica('Deixa Eu', 'Murilo Huff', 191);
+  Musica m3 = Musica('Bebe, Beija e Trai', 'Mayke e Rodrigo, Panda', 282);
 
   Playlist pl = Playlist('Sertanejo', m1, m2, m3);
   pl.exibirPlaylist();
