30902982e438893f454fbf3c41d327570a63cef8
André Felipe <a.n.d.r.e.11.ufcfc@gmail.com>
2026-04-06 21:28:48 -0300

    Adicionando questão 20 - main.dart

--- lib/questao20/main.dart
+++ lib/questao20/main.dart
@@ -4,9 +4,9 @@ import 'treinador.dart';
 void main() {
   Treinador ash = Treinador(
     'André',
-    Pokemon(01, 'Pikachu', 'Elétrico', 50, 100),
+    Pokemon(01, 'Pikachu', 'Elétrico', 90, 180),
     Pokemon(02, 'Charizard', 'Fogo/Voador', 80, 150),
-    Pokemon(03, 'Bulbasaur', 'Planta/Veneno', 45, 120),
+    Pokemon(03, 'Bulbasaur', 'Planta/Veneno', 65, 120),
   );
   ash.exibirTime();
 }
